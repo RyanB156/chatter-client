e3efb50479ec8c04bfbd369a0fe5053e73d969b8
Ryan Bressette <bressetteryan@gmail.com>
2020-08-07 10:15:36 -0400

    User story notes

--- src/app/login-register/login-register.component.ts
+++ src/app/login-register/login-register.component.ts
@@ -40,32 +40,55 @@ export class LoginRegisterComponent implements OnInit {
   /*
     TODO:
 
-    documentation
+    documentation comments
 
-    Login
-      Enter username √
-      Enter password √
-      Enter privateKey (or loaded from local storage if it exists) √
-      Click Submit √
+    Login - 
+      User enters username √
+      User enters password √
+      User enters privateKey (or it is loaded from local storage if it exists) √
+      User clicks "Submit" √
       Send username and password hash to server √
       Server checks for existence of username √
       Server checks password hash against the existing user with that username √
       Server generates random session key for the user, returns it and the user's public key, and tells the user they are logged in √
       Client saves session key. Also save current username in local storage. √
-      Client navigates to the home page and sees a list of conversations for the current user
+      -> Home
 
-    Register
-      Enter username and password √
-      Create key √
-      Send username, H(password), key to Server
-      Save private key in local storage
+    Register - User clicks on "Register" and enters information to create an account. Then login.
+      User enters username
+      User enters password
+      Client creates public and private keys √
+      Send username, H(password), and public key to Server √
+      Save private key in local storage and inform the user that they need to save it √
       -> Login
 
-    Encryption/Decryption
+    View list of conversations - User navigates to Home and sees a list of conversations for the current user
+      User is signed in
+      User clicks on "Home"
+      Client submits access request with username and session key
+      Server checks the session key against the username (Don't trust the client saying it is logged in)
+      If logged in, Server admits the request and returns the list of conversations. Else, return error
+      Client displays the conversations to the user
+
+    View messages in a conversation - User clicks on an active conversation from Home and sees all messages in that conversation
+      User is signed in and at Home, view a list of conversations
+      User clicks on a conversation
+      Client requests all messages from the conversation from Server, with username and session key
+      Server validates request
+      Server returns all messages in the conversation
+      Client displays messages in a nice format
+      
+
+      (conversation also stores the participants' public keys)
+
+    Create a new conversation - User clicks on "New Conversation"
+
+    Create a new message - User clicks on "New Message"
+
+
+    -Security Requirements
       Sender encrypts and signs message using RSA keys.
       Server and Receiver verify tag of messages and the Sender.
-
-    Home, ConversationView
       Require valid (matching) session key in all requests
   */
 
